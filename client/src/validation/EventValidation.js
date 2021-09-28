dd5596c443f5cd8473f33c7b13251d41795b4580
Sebastian <sebastian.susa95@gmail.com>
2021-09-29 01:53:42 +0200

    specified error messages

--- client/src/validation/EventValidation.js
+++ client/src/validation/EventValidation.js
@@ -1,8 +1,11 @@
 import * as yup from "yup";
 
 export const eventSchema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  email: yup.string().email().required(),
-  eventDate: yup.mixed().required(),
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last Name is required"),
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  eventDate: yup.mixed().required("Event date is required"),
 });
