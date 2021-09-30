db8c76cfd3e73d53e9768904a0bcb0f7fcafd6b9
Sebastian Susa <sebastian.susa95@gmail.com>
2021-10-01 01:41:59 +0200

    added backend tests

--- server/app.test.js
+++ server/app.test.js
@@ -1,7 +1,9 @@
 const request = require("supertest");
+const db = require("./db");
 const app = require("./app");
 
 describe("post /api/createEvent", () => {
+
   test("test with correct data", async () => {
     const response = await request(app).post("/api/createEvent").send({
       firstName: "First Name",
@@ -11,5 +13,42 @@ describe("post /api/createEvent", () => {
     });
     expect(response.status).toBe(201);
   });
-  describe("test with missing data", () => {});
+   
+    test("test with missing data", async () => {
+      const response = await request(app).post("/api/createEvent").send({});
+      expect(response.status).toBe(400);
+    });
+
+
+  describe("tests with incorrect data", () => {
+
+    test("test with incorrect first name", async () => {
+      const response = await request(app).post("/api/createEvent").send({      
+      firstName: "",
+      lastName: "Last Name",
+      email: "example@example.com",
+      eventDate: "dd/mm/YYYY",});
+      expect(response.status).toBe(400);
+    });
+
+    test("test with incorrect last name", async () => {
+      const response = await request(app).post("/api/createEvent").send({      
+      firstName: "First Name",
+      lastName: "",
+      email: "example@example.com",
+      eventDate: "dd/mm/YYYY",});
+      expect(response.status).toBe(400);
+    });
+
+    test("test with incorrect email", async () => {
+      const response = await request(app).post("/api/createEvent").send({      
+      firstName: "First Name",
+      lastName: "Last Name",
+      email: "example@example",
+      eventDate: "dd/mm/YYYY",});
+      expect(response.status).toBe(400);
+    });
+  });
 });
+
+
